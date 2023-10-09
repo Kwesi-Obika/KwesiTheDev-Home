a1ed79b2093901b65182354847ed73b269f5386c
Kwesi-Obika <Kwesi.Obika@Object-Oriented.dev>
2023-10-09 15:53:53 -0600

    converted footer image to SVG

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <img src="/KwesiTheDev-Logo.png" alt="Kwesi The Dev Logo- Graffiti Intitials KAO" className={styles.logo} />
+        <img src="/KwesiTheDev-Logo.svg" alt="Kwesi The Dev Logo- Graffiti Intitials KAO" className={styles.logo} />
       </footer>
     </>
   )
